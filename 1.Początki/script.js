f23702f79d3ea686701b46a453a2cc5342057378
laxus <laxus2323@o2.pl>
2020-10-13 12:30:36 +0200

    change comments for solutions

--- 1.Początki/script.js
+++ 1.Początki/script.js
@@ -1,33 +1,27 @@
-/*
-Zadanie 1
+/*Zadanie 1
 Wpisz w consolinastępującąinstrukcję:
-JavaScript console.log("Twoje imię");
- */
+JavaScript console.log("Twoje imię");*/
 
 console.log("Rafał");
 
-/*
-Zadanie 2
+/*Zadanie 2
 Wpisz w consoli następującą instrukcję i sprawdź ich wynik:
 ```JavaScript
 * 12 + 32;
 * 52 / 16;
-* 5 * 2;
- */
+* 5 * 2;*/
 console.log(12 + 32);
 console.log(52 / 16);
 console.log(5 * 2);
 
-/*
-Zadanie 3
+/*Zadanie 3
 Wpisz do konsoli i sprawdź co się wyświetli:
 
 ```
 *  "Hello World";
 *   Hello World;
 *  "Hello World;
-```
- */
+```*/
 console.log("Hello World");
 console.log(Hello World);
 console.log("Hello World);
